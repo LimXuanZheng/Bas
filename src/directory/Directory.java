b72fc5174426dea9e7edffa4a9f9fabb83e7af90
WongWeiXuan <wong_weixuan@hotmail.com>
2017-06-07 00:17:31 +0800

    commiting so i can sync

--- src/directory/Directory.java
+++ src/directory/Directory.java
@@ -2,11 +2,16 @@ package directory;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import database.DatabaseAccess;
 
 /**
  * Servlet implementation class Directory
@@ -18,25 +23,52 @@ public class Directory extends HttpServlet {
 	//private DataTable dataTable;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/jsp/directory.jsp").forward(request, response);
+        //request.getRequestDispatcher("/html/directory.html").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // Get and validate name.
         String name = request.getParameter("search");
-        
         // Get schoolClass
     	String schoolClass = request.getParameter("schoolClass");
+    	//Get Subject
+    	String subject = request.getParameter("subject");
     	
-    	String line = "<tr><td>" + name + "</td>" +
-    			"<td>" + schoolClass + "</td" +
-    			"<td>1234 5678</td><td>testing@testing.com</td></tr>";
-    	
+    	/*
+    	ResultSet rs = null;
         if (validateName(name)) {
-        	request.setAttribute("returnResult", line);
+        	DatabaseAccess dba = new DatabaseAccess();
+        	rs = dba.getDatabaseData("SELECT * FROM User WHERE Name='';");
         }
         
-        request.getRequestDispatcher("/jsp/directory.jsp").forward(request, response);
+        PrintWriter out = response.getWriter();
+        try {
+	        while(rs.next()){
+	        	//Retrieve by column name
+				String username  = rs.getString("Name");
+	            String DOB = rs.getString("DOB");
+	            int userID = rs.getInt("UserID");
+	
+	            //Display values
+	            out.println("Username: " + username + "<br>");
+	            out.println(", Password: " + DOB + "<br>");
+	            out.println(", UserID: " + userID + "<br>");
+	        }
+        }catch (SQLException e) {
+				e.printStackTrace();
+		}
+		
+		*/
+    	response.setContentType("text/html");
+    	PrintWriter out = response.getWriter();
+    	out.println("<html><head></head><body>");
+    	//Display values
+        out.println("Name: " + name + "<br>");
+        out.println("Class: " + schoolClass + "<br>");
+        out.println("Subject: " + subject + "<br>");
+        out.println("</body></html>");
+        
+        request.getRequestDispatcher("/html/directory.html").forward(request, response);
 	}
 
 	private boolean validateName(String name){
