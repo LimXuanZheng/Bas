82b4b7514bb1ffc72113cd58e84db934ce98bc3e
LimXuanZheng <limxuanzheng888@gmail.com>
2017-06-23 19:22:05 +0800

    finally

--- src/teacherSharing/teacherSharing.java
+++ src/teacherSharing/teacherSharing.java
@@ -1,48 +1,29 @@
-package fileUpload;
+package teacherSharing;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.bind.DatatypeConverter;
 
 import fileUpload.CryptoException;
-
+import fileUpload.encryption;
 
 /**
- * Servlet implementation class upload
+ * Servlet implementation class teacherSharing
  */
-@WebServlet("/uploads")
-public class upload extends HttpServlet {
+@WebServlet("/teachersharing")
+public class teacherSharing extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public upload() {
+    public teacherSharing() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -52,28 +33,17 @@ public class upload extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		String path = request.getParameter("datafile");
-		Path paths = Paths.get(path);
-		File f = paths.toFile();
-		out.println(path);
+		File decryptedFile = new File("document.decrypted");
 		String key = "Mary has one cat";
-        File encryptedFile = new File("document.encrypted");
-        try {
-        	encryption.encrypt(key, f, encryptedFile);
+        /*try {
+        	decryption.encrypt(key, encryptedFile, decryptedFile);
         } catch (CryptoException ex) {
             System.out.println(ex.getMessage());
             ex.printStackTrace();
             System.out.println("fail");
         }
-        
-		
-		
-		
+        */
 	}
-	
-	
-    
-	
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -81,9 +51,6 @@ public class upload extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		PrintWriter out = response.getWriter();
-		String filePath = request.getParameter("datafile");
-		out.println("hello");
 	}
 
 	/**
