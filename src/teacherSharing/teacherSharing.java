17f43f67bd36e1975f3369539a72c6ac0ebf31b5
LimXuanZheng <limxuanzheng888@gmail.com>
2017-07-03 22:55:55 +0800

    red

--- src/teacherSharing/teacherSharing.java
+++ src/teacherSharing/teacherSharing.java
@@ -38,13 +38,13 @@ public class teacherSharing extends HttpServlet {
     			+ 	"<head>"
     			+ 		"<meta charset='UTF-8'>"
     			+ 		"<meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>"
-    			+ 		"<link rel='stylesheet' href='css/teacherSharing.css'>"
     			+ 		"<!-- Font Mono-Sans -->"
     			+ 		"<link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>"
     			+ 		"<!-- Latest compiled and minified CSS -->"
     			+ 		"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>"
     			+ 		"<!-- Optional theme -->"
     			+ 		"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' integrity='sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp' crossorigin='anonymous'>"
+    			+		"<link rel='stylesheet' href='css/teacherSharing.css'>"
     			+		"<title>TeacherSharing</title>"
 		    	+		"<script>function goHome() {window.location('Home');}"
 				+		"</script>"
@@ -79,9 +79,11 @@ public class teacherSharing extends HttpServlet {
     			+					"<button class='button'>Shared Drive</button>"
     			+				"</div>"
     			+				"<div>"
-    			+					""
     			+				"</div>"
     			+			"</div>"
+    			+		"</div>"
+    			+	"</body>"
+    			+"</html>"
 				);
         
 	}
