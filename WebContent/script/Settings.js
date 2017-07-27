c891e2377f6e812efd77940f0bca665eff0ea258
Axomized <dr.que9@gmail.com>
2017-07-27 22:47:01 +0800

    User settings page in progress

---
+++ WebContent/script/Settings.js
@@ -0,0 +1,10 @@
+function showChangeEmailDiv() {
+	var changeEmailDiv = document.getElementById("changeEmailDiv");
+	if (changeEmailDiv.style.display = "none") {
+		changeEmailDiv.style.display = "block";
+	}
+	else {
+		changeEmailDiv.style.display = "none";
+	}
+	
+}
