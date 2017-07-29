59ce50728f783d507a33e940d1643e93150d06c1
Axomized <dr.que9@gmail.com>
2017-07-29 20:19:26 +0800

    User settings design

--- WebContent/script/Settings.js
+++ WebContent/script/Settings.js
@@ -8,3 +8,17 @@ function showChangeEmailDiv() {
 	}
 	
 }
+
+function showChangePassDiv() {
+	var changePassDiv = document.getElementById("changePassDiv");
+	var editPassLinkDiv = document.getElementById("editPassLinkDiv");
+	var editPassLink = document.getElementById("editPassLink");
+	if (changePassDiv.style.display = "none") {
+		changePassDiv.style.display = "block";
+		editPassLink.innerHTML = "Click to close"
+	}
+	else {
+		changePassDiv.style.display = "none";
+		editPassLink.innerHTML = "Click to open"
+	}
+}
