28721b159639c18e52a9a3d294beb618e42bb173
Axomized <dr.que9@gmail.com>
2017-08-07 15:11:45 +0800

    ZZZ

--- src/login/LoginServlet.java
+++ src/login/LoginServlet.java
@@ -49,7 +49,7 @@ public class LoginServlet extends HttpServlet {
 			while (sc.hasNextLine()) {
 				arr.add(sc.nextLine());
 			}
-			arr.set(3, "<File name=\"MyFile\" filename=\"" + logFile + "\\Documents\\GitHub\\purplebackup\\AuditLog.log" + "\">");
+			arr.set(3, "<File name=\"MyFile\" filename=\"" + logFile + "\\Documents\\GitHub\\purplebackup\\src\\auditLogFile\\AuditLog.log" + "\">");
 			FileWriter fw = new FileWriter(file);
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter outFile = new PrintWriter(bw);
